6c4a25c841f1a50cca374f49b7c66fa90a20abb4
bobbyduhbrain <Mike@MIKEs-MacBook-Pro.local>
2014-05-21 15:37:54 -0400

    Moved gimmick to wrestler subfolder and included it at the top of wrestler.rb

--- lib/ewruby/game_world/wrestler.rb
+++ lib/ewruby/game_world/wrestler.rb
@@ -1,6 +1,7 @@
 require_relative "wrestler/attributes"
-require_relative "wrestler/statistics"
 require_relative "wrestler/injury"
+require_relative "wrestler/gimmick"
+require_relative "wrestler/statistics"
 
 class Wrestler
   attr_accessor :attributes, :contracts, :first_name, :gimmicks, :last_name, :moveset
