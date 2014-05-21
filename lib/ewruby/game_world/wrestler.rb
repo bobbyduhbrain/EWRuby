ae5d3464a59865dd6203992034ba62a360a3511a
bobbyduhbrain <Mike@MIKEs-MacBook-Pro.local>
2014-05-21 16:27:11 -0400

    Refactored wrestler again.

--- lib/ewruby/game_world/wrestler.rb
+++ lib/ewruby/game_world/wrestler.rb
@@ -1,30 +1,30 @@
 Dir["./lib/ewruby/game_world/wrestler/*.rb"].each {|file| require file }
 
 class Wrestler
-  attr_accessor :data, :statistics, :attributes
+  attr_accessor :data
 
   def initialize
     @data ||= {
+      attributes: {
+        fonz_factor: false,
+        menacing: false,
+        shoot_fighting: false,
+        superstar_look: false
+      },
       contracts: Array.new,
       first_name: "",
       gimmicks: Array.new,
       injuries: Array.new,
       last_name: "",
-      moveset: Array.new
-    }
-    @statistics ||= {
-      brawl: 0,
-      charisma: 0,
-      overness: 0,
-      power: 0,
-      speed: 0,
-      technical: 0
-    }
-    self.attributes ||= {
-      fonz_factor: false,
-      menacing: false,
-      shoot_fighting: false,
-      superstar_look: false
+      moveset: Array.new,
+      statistics: { 
+        brawl: 0,
+        charisma: 0,
+        overness: 0,
+        power: 0,
+        speed: 0,
+        technical: 0
+      }
     }
   end
 end
