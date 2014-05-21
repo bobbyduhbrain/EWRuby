4fb6596c58774f8e72ea8d0be539779b89877f89
bobbyduhbrain <Mike@MIKEs-MacBook-Pro.local>
2014-05-21 15:35:02 -0400

    Gave wrestlers injuries (lol)

--- lib/ewruby/game_world/wrestler.rb
+++ lib/ewruby/game_world/wrestler.rb
@@ -1,5 +1,6 @@
 require_relative "wrestler/attributes"
 require_relative "wrestler/statistics"
+require_relative "wrestler/injury"
 
 class Wrestler
   attr_accessor :attributes, :contracts, :first_name, :gimmicks, :last_name, :moveset
@@ -7,6 +8,6 @@ class Wrestler
   def initialize
     @attributes = Attributes.new
     @first_name, @last_name = "", ""
-    @contracts, @gimmicks, @moveset = Array.new(3) { [] }
+    @contracts, @gimmicks, @moveset, @injuries = Array.new(4) { [] }
   end
 end
